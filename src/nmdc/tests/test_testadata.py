b9d34844eaf3e0d1fcba05fe6167d699dc565ce9
hubin-keio <8225406+hubin-keio@users.noreply.github.com>
2020-12-21 14:59:05 -0700

    Added OBO translation that conversts features like "CDS" to SO terms

--- src/nmdc/tests/test_testadata.py
+++ src/nmdc/tests/test_testadata.py
@@ -1,15 +1,26 @@
 import io
+import os
 import unittest
+import obonet
 from BCBio import GFF
-from nmdc.metadata.schema import GenomeFeature
+from nmdc.metadata import schema
+
 
 class testMetadata(unittest.TestCase):
     def test_GenomeFeature(self):
         """
-        Test import GenomeFeature from GFF3 file. The example gff line is from here:
+        Test import GenomeFeature from GFF3 file.
+        See the issue below for details:
         https://github.com/microbiomedata/nmdc-metadata/issues/184
         """
         gff_str = 'Ga0185794_41\tGeneMark.hmm-2 v1.05\tCDS\t48\t1037\t56.13\t+\t0\tID=Ga0185794_41_48_1037;translation_table=11;start_type=ATG;product=5-methylthioadenosine/S-adenosylhomocysteine deaminase;product_source=KO:K12960;cath_funfam=3.20.20.140;cog=COG0402;ko=KO:K12960;ec_number=EC:3.5.4.28,EC:3.5.4.31;pfam=PF01979;superfamily=51338,51556'
+
+        SOOBO = os.path.join(os.path.dirname(schema.__file__),
+                             'so-simple.obo')
+        sograph = obonet.read_obo(SOOBO)
+        sddict = {data.get('name'): id_
+                  for id_, data in sograph.nodes(data=True)}
+
         print(f'Test GenomeFeature...\n\nInput:\n\t{gff_str}\n')
 
         with io.StringIO() as fh:
@@ -18,7 +29,11 @@ class testMetadata(unittest.TestCase):
             for rec in GFF.parse(fh):
                 for feature in rec.features:
                     feature_id = feature.id
-                    feature_type = feature.type  # FIXME
+                    feature_type = '**' + feature.type  # FIXME
+                    if feature_type in sddict.keys():
+                        feature_type_so = sddict[feature_type]
+                    else:
+                        feature_type_so = None
                     feature_start = int(feature.location.start)
                     feature_end = int(feature.location.end)
                     feature_strand = feature.location.strand
@@ -31,12 +46,12 @@ class testMetadata(unittest.TestCase):
                     print(f'Features from GFF input for {feature.id}:')
                     for qual in feature.qualifiers:
                         print(f'\t{qual} => {feature.qualifiers[qual]}')
-                    nmdc_gf = GenomeFeature(
+                    nmdc_gf = schema.GenomeFeature(
                         seqid=f'NMDC:{rec.id}',  # record id
                         start=feature_start,
                         end=feature_end,
                         strand=feature_strand,
-                        # type=feature_type, # FIXME
+                        type={'term': feature_type_so},
                         encodes=f'NMDC:{feature_id}'  # feature id # FIXME
                     )
                     self.assertEqual(nmdc_gf.seqid,
@@ -44,7 +59,7 @@ class testMetadata(unittest.TestCase):
                     self.assertEqual(nmdc_gf.start, 48-1)
                     self.assertEqual(nmdc_gf.end, 1037)
                     self.assertEqual(nmdc_gf.strand, '+')
-                    # self.assertEqual(nmdc_gf.type, 'CDS')
+                    # self.assertEqual(nmdc_gf.type, 'SO:0000316')
                     # self.assertEqual(nmdc_gf.encodes,
                     #                  'NMDC:Ga0185794_41_48_1037')
                     print('\nFrom NMDC GenomeFeature\n')
